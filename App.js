ccf1bc84e5d4249975111c0d89290d3824bbc875
remi <remi.petit93370@gmail.com>
2023-10-23 15:06:59 +0200

    init

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style={"auto"} />
+      <div className={styles.haut}>
+        <p>test</p>
+      </div>
+      <div className={styles.bas}>
+        <p>test</p>
+      </div>
     </View>
   );
 }
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  haut: {
+    backgroundColor: 'red',
+  },
+  bas: {
+    
   },
 });
